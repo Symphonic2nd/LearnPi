37ac99c77f4d42c97ac9a7a8198b8a75a3be6e31
Symphonic2nd <fyodorovich@icloud.com>
2020-01-22 20:00:15 -0600

    First commmit!

---
+++ LearnPi/src/learnpi/LearnPi.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package learnpi;
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+/**
+ *
+ * @author Vicky
+ */
+public class LearnPi extends JFrame{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new LearnPi();
+    }
+    
+    
+    public LearnPi() {
+        
+    }
+}
